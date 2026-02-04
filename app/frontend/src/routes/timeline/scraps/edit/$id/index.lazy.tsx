301e5771c53a60f6ca3457de1c62e95f5d930acc
haruki26 <h826.a519@gmail.com>
2026-02-04 04:01:46 +0000

    add: エディタを共通のものとして定義しeditページを作成

--- app/frontend/src/routes/timeline/scraps/edit/$id/index.lazy.tsx
+++ app/frontend/src/routes/timeline/scraps/edit/$id/index.lazy.tsx
@@ -1,9 +1,24 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { useFetchScrapDetailOptions } from '@/api/routes/scraps'
+import { useUpdateScrapForm } from '@/features/timeline/scraps/edit/hooks/useUpdateScrapForm'
+import { useFetchTagsOptions } from '@/api/routes/tags'
+import ScrapEditor from '@/features/timeline/scraps/components/ScrapEditor'
 
 export const Route = createLazyFileRoute('/timeline/scraps/edit/$id/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/timeline/scraps/edit/$id/"!</div>
+  const { id } = Route.useParams()
+  const { data } = useSuspenseQuery(useFetchScrapDetailOptions(id))
+  const { data: tags } = useSuspenseQuery(useFetchTagsOptions())
+
+  const { form } = useUpdateScrapForm(id, {
+    title: data.title,
+    body: data.body,
+    tags: data.tagScraps.map((tagScrap) => tagScrap.tagId),
+  })
+
+  return <ScrapEditor form={form} tags={tags} submitLabel="更新" />
 }
--- app/frontend/src/routes/timeline/scraps/edit/$id/index.tsx
+++ app/frontend/src/routes/timeline/scraps/edit/$id/index.tsx
@@ -1,7 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useFetchScrapDetailOptions } from '@/api/routes/scraps'
+import { useFetchTagsOptions } from '@/api/routes/tags'
 
 export const Route = createFileRoute('/timeline/scraps/edit/$id/')({
-  loader: () => {
-    console.log('Loading /timeline/scraps/edit/$id/ route')
+  loader: ({ context, params }) => {
+    context.queryClient.ensureQueryData(useFetchTagsOptions())
+    context.queryClient.ensureQueryData(useFetchScrapDetailOptions(params.id))
   },
 })
